e4c754eacea4727a61cff197f99ee61cc86c7524
perezcalderondulcemariam3s1-gif <perez.calderon.dulce.maria.m3s1@gmail.com>
2026-02-12 21:46:37 -0600

    Add test case for duplicate primary key insertion
    Add test case for inserting a duplicate cake ID

--- tests/test_cases.sql
+++ tests/test_cases.sql
@@ -1,1 +1,4 @@
+-- Prueba: Intentar meter un pastel con un ID que ya existe
+-- Esto debería marcar ERROR porque el cake_id es PRIMARY KEY
+INSERT INTO cakes (cake_id, cake_name, price) VALUES (1, 'Pastel Repetido', 100.00);
 
