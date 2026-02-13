73c687be47adb8831c7793bfc234711520483e3f
perezcalderondulcemariam3s1-gif <perez.calderon.dulce.maria.m3s1@gmail.com>
2026-02-12 21:45:34 -0600

    Add seller_user with permissions for cakes

--- src/03_users.sql
+++ src/03_users.sql
@@ -1,1 +1,6 @@
+-- Creamos un usuario para el vendedor
+CREATE USER 'seller_user'@'localhost' IDENTIFIED BY 'Pastel123';
+
+-- Le damos permiso solo para ver pasteles y vender
+GRANT SELECT, INSERT ON cakes TO 'seller_user'@'localhost';
 
