07cff5335ea71c614531b04294927f44f01ddb6c
perezcalderondulcemariam3s1-gif <perez.calderon.dulce.maria.m3s1@gmail.com>
2026-02-12 21:43:52 -0600

    Insert initial data for cakes and customers

--- src/02_seed.sql
+++ src/02_seed.sql
@@ -1,1 +1,11 @@
+-- Metemos 3 pasteles
+INSERT INTO cakes (cake_id, cake_name, price) VALUES 
+(1, 'Chocolate Cake', 350.00),
+(2, 'Vanilla Strawberry', 300.00),
+(3, 'Red Velvet', 400.00);
+
+-- Metemos 2 clientes
+INSERT INTO customers (customer_id, first_name, last_name, phone_number) VALUES 
+(1, 'Dulce', 'Perez', '1234567890'),
+(2, 'Wendy', 'Hernandez', '0987654321');
 
